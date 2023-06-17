e3d647c04a6d4d0f78e35beec08aa46624a0820a
7marcelo7 <martinsmathiasmarcelo@gmail.com>
2023-06-17 17:52:19 -0300

    adicionado os exemplos de variaveis e seus tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,40 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos de dados lógicos
+        boolean verdadeiro = true; // Tipo lógico (booleano)
+        boolean falso = false; // Tipo lógico (booleano)
+
+        // Tipos de dados literais
+        char caractere = 'A'; // Tipo literal (caractere)
+        String texto = "Olá, mundo!"; // Tipo literal (cadeia de caracteres)
+
+        // Tipos de dados inteiros
+        byte valorByte = 10; // Tipo inteiro (byte)
+        short valorShort = 100; // Tipo inteiro (short)
+        int valorInt = 1000; // Tipo inteiro (int)
+        long valorLong = 100000L; // Tipo inteiro (long)
+
+        // Tipos de dados reais
+        float valorFloat = 3.14f; // Tipo real (float)
+        double valorDouble = 2.71828; // Tipo real (double)
+
+        // Exibindo os valores das variáveis
+        System.out.println("Tipos de Dados Lógicos:");
+        System.out.println("Verdadeiro: " + verdadeiro);
+        System.out.println("Falso: " + falso);
+
+        System.out.println("\nTipos de Dados Literais:");
+        System.out.println("Caractere: " + caractere);
+        System.out.println("Texto: " + texto);
+
+        System.out.println("\nTipos de Dados Inteiros:");
+        System.out.println("Byte: " + valorByte);
+        System.out.println("Short: " + valorShort);
+        System.out.println("Int: " + valorInt);
+        System.out.println("Long: " + valorLong);
+
+        System.out.println("\nTipos de Dados Reais:");
+        System.out.println("Float: " + valorFloat);
+        System.out.println("Double: " + valorDouble);
     }
 }
