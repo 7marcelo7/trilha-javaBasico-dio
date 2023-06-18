815979526a683d9cc145215a7fb2c0d3b545ed0c
7marcelo7 <martinsmathiasmarcelo@gmail.com>
2023-06-18 16:26:08 -0300

    adicionando exemplos de metodos

---
+++ Metodos/src/ExMetodos.java
@@ -0,0 +1,20 @@
+public class ExMetodos {
+    public static void main(String[] args) throws Exception {
+        // exemplos de como declarar um mentodo
+
+        /*
+         * Ex 1.:
+         * public int somar(int a, int b) {
+         * int resultado = a + b;
+         * return resultado;
+         * }
+         */
+
+        /* Ex 2.:
+         * public void exibirMensagem(String mensagem) {
+         * System.out.println("Mensagem: " + mensagem);
+         * }
+         * 
+         */
+    }
+}
